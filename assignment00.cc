c214de962a029d54e29f15f04606a21baf701166
Nikolozi Gatov <gatovnikolay@gmail.com>
2022-01-30 20:50:29 -0600

    Added starter code with a Doc Box

---
+++ assignment00.cc
@@ -0,0 +1,17 @@
+/*  Nikolozi Gatov
+	Z1884744
+	CSCI 480 - PE1 Principles Operating Systems
+	
+	I certify that this is my own work and where appropriate an extension
+	of the starter code provided for the assignment.
+*/
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    return 0;
+}
